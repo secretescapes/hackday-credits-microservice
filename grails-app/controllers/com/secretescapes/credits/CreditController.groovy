f1b1b1595634d23fb8f0148469fcfc2e446a0191
Michal Szulc <michal.szulc@secretescapes.com>
2021-06-10 16:22:59 +0300

    Refacrtorized getAuhenticatedUser() + added unit test for controller

--- grails-app/controllers/com/secretescapes/credits/CreditController.groovy
+++ grails-app/controllers/com/secretescapes/credits/CreditController.groovy
@@ -6,20 +6,21 @@ import org.springframework.http.HttpStatus
 
 class CreditController {
 	static responseFormats = ['json']
-	static allowedMethods = [index: 'GET', redeemCredit: 'POST']
+	static allowedMethods = [redeemCredit: 'POST']
 
 	CreditService creditService
 
 	def index() {
+		User user
 		try {
-			String userId = request.getHeader("X-userId")
-			User user = User.read(userId)
+			user = getAuhenticatedUser()
 			if (!user) {
+				log.info "${controllerName}.${actionName}: was't authenticated"
 				render([status: HttpStatus.FORBIDDEN])
 				return
 			}
 
-			Currency currency = params.currency ?: Currency.getInstance("GBP")
+			Currency currency = params.currency ? Currency.getInstance(params.currency) : Currency.getInstance("GBP")
 			BigDecimal amount = creditService.sumAmountOfAvailableCreditsByUserAndCurrency(user, currency)
 			[amount: amount, currency: currency]
 		} catch (ex) {
@@ -30,23 +31,31 @@ class CreditController {
 	}
 
 	def redeemCredit(RedeemCreditCommand command) {
+		User user
 		try {
-			String userId = request.getHeader("X-userId")
-			User user = User.read(userId)
+			user = getAuhenticatedUser()
 			if (!user) {
+				log.info "${controllerName}.${actionName}: was't authenticated"
 				render([status: HttpStatus.FORBIDDEN])
 				return
 			}
 			if (!command.validate()) {
+				log.info "${controllerName}.${actionName}: was't validated"
 				render([status: HttpStatus.BAD_REQUEST])
 				return
 			}
 			render([status: HttpStatus.OK])
 		} catch (InsufficientAvailableCredit e) {
+			log.info "${controllerName}.${actionName}: throw InsufficientAvailableCredit for User-${user?.id} and ${command.currency}:${command.requestedCreditAmount}"
 			render([status: HttpStatus.CONFLICT])
 		} catch (e) {
 			log.warn "${controllerName}.${actionName}: throw ${ex.message}", ex
 			render([status: HttpStatus.INTERNAL_SERVER_ERROR])
 		}
+	}
+
+	private User getAuhenticatedUser(){
+		String userId = request.getHeader("X-userId")
+		return User.read(userId)
 	}
 }
--- src/test/groovy/com/secretescapes/credits/CreditControllerSpec.groovy
+++ src/test/groovy/com/secretescapes/credits/CreditControllerSpec.groovy
@@ -1,18 +1,33 @@
 package com.secretescapes.credits
 
+import grails.testing.gorm.DataTest
 import grails.testing.web.controllers.ControllerUnitTest
 import spock.lang.Specification
 
-class CreditControllerSpec extends Specification implements ControllerUnitTest<CreditController> {
+class CreditControllerSpec extends Specification implements ControllerUnitTest<CreditController>, DataTest {
 
+	CreditService creditService
     def setup() {
+	    creditService = Mock(CreditService)
+	    controller.creditService = creditService
+
+	    mockDomains(User)
     }
 
-    def cleanup() {
-    }
+	def cleanup() {
+	}
 
-    void "test something"() {
-        expect:"fix me"
-        true == false
-    }
+	void "index: get proper amount of credits for specific user and currency"() {
+		given:
+			User user = new User().save(validate: false)
+			request.addHeader("X-userId", user.id)
+
+		when:
+			def result = controller.index()
+
+		then:
+			1 * creditService.sumAmountOfAvailableCreditsByUserAndCurrency(_, _) >> 10
+			result.amount == 10
+			result.currency == Currency.getInstance("GBP")
+	}
 }
