dece1ac1cbc028636c58ede29e40f96e6837d910
Mario <mario.martinez@secretescapes.com>
2021-06-11 13:57:47 +0200

    create test users

--- grails-app/init/com/secretescapes/credits/BootStrap.groovy
+++ grails-app/init/com/secretescapes/credits/BootStrap.groovy
@@ -5,15 +5,14 @@ class BootStrap {
 	def init = { servletContext ->
 
 		User.withTransaction {
-			User user = User.read(1)
-			if (!user) {
-				user = User.create().save(flush: true)
-				user.addToCredits(new Credit(user: user, amount: 100))
-				user.addToCredits(new Credit(user: user, amount: 50))
-				user.addToCredits(new Credit(user: user, amount: 50, status: CreditStatus.USED))
-				user.addToCredits(new Credit(user: user, amount: 150, status: CreditStatus.DELETED))
-				user.addToCredits(new Credit(user: user, amount: 50, expiresOn: new Date("2019/01/01")))
+			(1..100).each {
+				User user = User.read(it)
+				if (!user) {
+					user = User.create().save(flush: true)
+					user.addToCredits(new Credit(user: user, amount: 500))
+				}
 			}
+
 		}
 
 	}
